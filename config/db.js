69947f3b92cd884d4e529f7e6b142404eb92a630
Bhankid <alfredfianyo50@gmail.com>
2024-10-21 11:48:12 +0000

    update 1.232.2

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  // Optional: Add other settings like timeout, connection limit, etc., if needed
+
 });
 
 // Function to connect to the database
